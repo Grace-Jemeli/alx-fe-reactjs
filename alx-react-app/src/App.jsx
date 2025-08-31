a5b3a5583069b624f5224d1a20fa3a370c218c47
Grace Jemeli <graceyjemeli@gmail.com>
2025-08-31 23:06:00 +0300

    Add user profile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
@@ -8,11 +9,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
