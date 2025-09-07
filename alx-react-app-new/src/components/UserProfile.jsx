a854765c711594b26c7d3ac5657680a504762b94
Grace Jemeli <graceyjemeli@gmail.com>
2025-09-07 16:21:24 +0300

    add style to components

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,9 +2,9 @@
 const UserProfile = (props) => {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2 style={{ color: 'blue' }}>{props.name}</h2>
+      <p>Age:<span style={{ fontWeight: 'bold' }}></span> {props.age}</p>
+      <p style={{ color: 'red' }}>Bio: {props.bio}</p>
     </div>
   );
 };
