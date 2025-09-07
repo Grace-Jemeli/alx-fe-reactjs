41c52d2bc99ccc98483f210cf7ff804e588d78bb
Grace Jemeli <graceyjemeli@gmail.com>
2025-09-07 22:50:45 +0300

    fix. update userprof to use context

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,8 +7,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
 
 
 
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,16 @@
-// src/components/UserProfile.jsx
-const UserProfile = (props) => {
+import { useContext } from 'react';
+import UserContext from './UserContext';
+import UserInfo from './UserInfo';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>User Profile</h2>
+      <UserInfo userData={userData} />
     </div>
   );
-};
+}
 
 export default UserProfile;
