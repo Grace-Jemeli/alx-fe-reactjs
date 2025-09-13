d35b50e79e7500e6c1358aebdc2bfb83bf8f528e
Grace Jemeli <graceyjemeli@gmail.com>
2025-09-13 05:56:13 +0300

    Update files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <div>
               <h1>Recipe Sharing App</h1>
               <AddRecipeForm />
+              <SearchBar />
               <RecipeList />
             </div>
           }
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,23 @@
+import React from 'react';
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
   const recipes = useRecipeStore((state) => state.recipes);
+
+  const recipesToShow = filteredRecipes.length > 0 ? filteredRecipes : recipes;
 
   return (
     <div>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipe List</h2>
+      <ul>
+        {recipesToShow.map((recipe) => (
+          <li key={recipe.id}>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
