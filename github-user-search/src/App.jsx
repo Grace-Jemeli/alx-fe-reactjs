481a4b720194e029d900222cb8616062b236b33b
Grace Jemeli <graceyjemeli@gmail.com>
2025-09-19 05:46:01 +0300

    Set up Github-user-search project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Github user search</h1>
+        <p>Welcome, the App is set up correctly!</p>
+      </div>
+    </>
+  );
+}
+
+
+
+
+export default App;
+
+
