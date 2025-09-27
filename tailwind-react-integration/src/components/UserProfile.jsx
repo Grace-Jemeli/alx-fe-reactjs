f5739021f3b8403c8c045a0cb9afffd84455a4aa
Grace Jemeli <graceyjemeli@gmail.com>
2025-09-27 06:59:31 +0300

    update few changes on userprof

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100  mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 max-w-sm md:max-w-sm">
+    <div className="bg-gray-100 sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 max-w-sm md:max-w-sm">
       <img className="rounded-full mx-auto w-24 h-24 md:w-36 md:h-36" src="https://via.placeholder.com/150" alt="User" />
       <h1 className="md:text-xl text-blue-800 my-4 text-lg">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
